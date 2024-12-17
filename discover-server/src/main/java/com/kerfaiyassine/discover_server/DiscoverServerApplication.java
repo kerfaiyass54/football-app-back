1ebc4e2d0780ae4daf9ef20aaa8117286807e58f
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-17 20:18:46 +0100

    Added eureka server

---
+++ discover-server/src/main/java/com/kerfaiyassine/discover_server/DiscoverServerApplication.java
@@ -0,0 +1,13 @@
+package com.kerfaiyassine.discover_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscoverServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoverServerApplication.class, args);
+	}
+
+}
