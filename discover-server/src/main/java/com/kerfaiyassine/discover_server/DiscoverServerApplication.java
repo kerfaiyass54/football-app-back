f01e836e57150931a0b3786a787e7044f6e41374
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-17 20:25:05 +0100

    Added eureka server configs

--- discover-server/src/main/java/com/kerfaiyassine/discover_server/DiscoverServerApplication.java
+++ discover-server/src/main/java/com/kerfaiyassine/discover_server/DiscoverServerApplication.java
@@ -2,8 +2,11 @@ package com.kerfaiyassine.discover_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class DiscoverServerApplication {
 
 	public static void main(String[] args) {
