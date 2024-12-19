3196e6a0fe6ff01404d78f8cc402d486102f9647
kerfaiyass54 <yassine.kerfai@etudiant-enit.utm.tn>
2024-12-19 21:50:21 +0100

    Added new microservices

---
+++ supporter/src/main/java/com/kerfaiyassine/supporter/SupporterApplication.java
@@ -0,0 +1,13 @@
+package com.kerfaiyassine.supporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupporterApplication.class, args);
+	}
+
+}
