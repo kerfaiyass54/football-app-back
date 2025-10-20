90fc14a814e2f7e3cc4a1c376751b35076c10208
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-10-20 20:56:33 +0100

    Added referee entity

---
+++ referee/src/main/java/com/kerfaiyassine/referee/Referee.java
@@ -0,0 +1,25 @@
+package com.kerfaiyassine.referee;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Referee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private Integer Id;
+
+    @Column(name="name", nullable = false, unique = true)
+    private String name;
+
+    @Column(name="nationality", nullable = false)
+    private String nationality;
+}
