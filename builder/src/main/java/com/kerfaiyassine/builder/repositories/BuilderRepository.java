ad7c92c0fb2d97ae6fe168b5acbf7a0acf60baf5
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-28 18:30:12 +0100

    Added builder repository

---
+++ builder/src/main/java/com/kerfaiyassine/builder/repositories/BuilderRepository.java
@@ -0,0 +1,7 @@
+package com.kerfaiyassine.builder.repositories;
+
+import com.kerfaiyassine.builder.entities.Builder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BuilderRepository extends JpaRepository<Builder, Integer> {
+}
---
+++ builder/src/main/java/com/kerfaiyassine/builder/services/BuilderService.java
@@ -0,0 +1,4 @@
+package com.kerfaiyassine.builder.services;
+
+public class BuilderService {
+}
