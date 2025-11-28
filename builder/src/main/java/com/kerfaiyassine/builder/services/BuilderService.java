4065d05141ddf31171d1dc481752455b488f7948
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-28 19:13:18 +0100

    Added builder's service

--- builder/src/main/java/com/kerfaiyassine/builder/services/BuilderService.java
+++ builder/src/main/java/com/kerfaiyassine/builder/services/BuilderService.java
@@ -1,4 +1,16 @@
 package com.kerfaiyassine.builder.services;
 
+
+import com.kerfaiyassine.builder.repositories.BuilderRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class BuilderService {
+
+    private final BuilderRepository builderRepository;
+
 }
