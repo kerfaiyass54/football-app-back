14c0d52616d71d6ba6db737487c20a6a4c28fd5e
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-28 20:00:31 +0100

    Added supporter's entities

---
+++ supporter/src/main/java/com/kerfaiyassine/supporter/entities/Supporter.java
@@ -0,0 +1,32 @@
+package com.kerfaiyassine.supporter.entities;
+
+
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document(collection = "supporter")
+public class Supporter {
+
+    @Id
+    private long id;
+    private String name;
+    private String nationality;
+    private long locationId;
+
+}
