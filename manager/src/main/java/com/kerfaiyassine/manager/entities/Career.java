649933d7f41a5413f37ba8a6e2d2b653d929e779
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-28 19:46:49 +0100

    Added career's entity

---
+++ manager/src/main/java/com/kerfaiyassine/manager/entities/Career.java
@@ -0,0 +1,30 @@
+package com.kerfaiyassine.manager.entities;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.Builder;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document(collection = "career")
+public class Career {
+
+    @Id
+    private long id;
+    private int duration;
+    private int yearStart;
+    private String status;
+    private boolean renewable;
+
+
+}
