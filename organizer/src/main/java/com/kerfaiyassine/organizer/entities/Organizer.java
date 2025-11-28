a240126cf607328f5c5cc52d761ed9ec051a5eba
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-28 19:15:29 +0100

    Added organizer entity

--- organizer/src/main/java/com/kerfaiyassine/organizer/entities/Organizer.java
+++ organizer/src/main/java/com/kerfaiyassine/organizer/entities/Organizer.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.Builder;
@@ -21,6 +22,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @Entity
+@Table(name = "organizer")
 public class Organizer {
 
     @Id
