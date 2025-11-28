bb70fb4eb89f2df99226d8b0a056391accd5b453
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-11-28 19:36:11 +0100

    Added player's entity

--- player/src/main/java/com/kerfaiyassine/player/entities/Player.java
+++ player/src/main/java/com/kerfaiyassine/player/entities/Player.java
@@ -4,12 +4,19 @@ package com.kerfaiyassine.player.entities;
 import com.kerfaiyassine.player.enums.PlayerPosition;
 import com.kerfaiyassine.player.enums.PlayerSituation;
 import com.kerfaiyassine.player.enums.PlayerStatus;
-import lombok.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
+import lombok.Builder;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 
+
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
