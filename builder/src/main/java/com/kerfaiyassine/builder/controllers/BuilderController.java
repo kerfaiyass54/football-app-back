98f831ee0a88a5d21a21cee6ee8d2f7c8db2478d
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2025-12-30 16:54:55 +0100

    Added controllers for builder

--- builder/src/main/java/com/kerfaiyassine/builder/controllers/BuilderController.java
+++ builder/src/main/java/com/kerfaiyassine/builder/controllers/BuilderController.java
@@ -1,13 +1,16 @@
 package com.kerfaiyassine.builder.controllers;
 
 
+import com.kerfaiyassine.builder.DTOs.BuilderDTO;
+import com.kerfaiyassine.builder.entities.Builder;
+import com.kerfaiyassine.builder.enums.Expertise;
 import com.kerfaiyassine.builder.services.BuilderService;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/builder")
@@ -19,6 +22,44 @@ public class BuilderController {
     public BuilderController(BuilderService builderService) {
         this.builderService = builderService;
     }
+
+    @PostMapping("/")
+    public ResponseEntity<Builder> createBuilder(@RequestBody BuilderDTO builderDTO){
+        Builder builder = builderService.createBuilder(builderDTO);
+        return new ResponseEntity<>(builder, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BuilderDTO> getBuilder(@PathVariable Integer id){
+        BuilderDTO builderDTO = builderService.getBuilderById(id);
+        return new ResponseEntity<>(builderDTO, HttpStatus.OK);
+    }
+
+    @GetMapping("/{nationality}")
+    public ResponseEntity<List<BuilderDTO>> getBuildersByNationality(@PathVariable String nationality){
+        List<BuilderDTO> builders = builderService.getBuilderByNationality(nationality);
+        return new ResponseEntity<>(builders, HttpStatus.OK);
+    }
+
+    @GetMapping("/{expertise}")
+    public ResponseEntity<List<BuilderDTO>> getBuildersByExpertise(@PathVariable Expertise expertise){
+        List<BuilderDTO> builders = builderService.getBuilderByExpertise(expertise);
+        return new ResponseEntity<>(builders, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}/{price}")
+    public ResponseEntity<Void> updateBuilder(@PathVariable Integer id, @PathVariable Integer price){
+        builderService.updateBuilderPrice(id, price);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<Page<BuilderDTO>> getAllBuilders(@RequestParam(defaultValue = "0") int page,
+                                                           @RequestParam(defaultValue = "5") int size){
+        Page<BuilderDTO> builders = builderService.getAllBuilders(page, size);
+        return new ResponseEntity<>(builders, HttpStatus.OK);
+    }
+
 
 
 }
