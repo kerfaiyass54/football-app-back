9d317397d3095f0e1fedceba454bb6ed59471c4f
Yassine Kerfai <yassine.kerfai@etudiant-enit.utm.tn>
2026-02-24 03:06:29 +0100

    Added config and fixings to organizers

--- databases_postgresql/init/organizer.sql
+++ databases_postgresql/init/organizer.sql
@@ -1,4 +1,4 @@
 CREATE TABLE organizers (
-                            id INTEGER PRIMARY KEY,
+                            id SERIAL PRIMARY KEY,
                             name VARCHAR(100) NOT NULL UNIQUE
 );
